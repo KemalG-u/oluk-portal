05972cd6293f6fbab7e2b0460b5edbdeb64482a5
KemalG-u <hatay61@gmail.com>
2025-12-19 13:02:56 +0000

    Fix: basePath kaldırıldı (custom domain için)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/oluk-portal',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
